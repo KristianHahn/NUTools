0174da79dc2eac2fa8b949ab68d68f69c1b12bc1
Kristian Hahn <khahn@cern.ch>
2020-02-20 17:33:30 -0600

    bit width ...

--- buffer_gen_TE.cc
+++ buffer_gen_TE.cc
@@ -29,25 +29,57 @@ vector<string> split(const string& s, char delimiter)
 int main(int argc, char ** argv) {
 
   vector<ifstream*> stubinnerfiles, stubouterfiles;
-  stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE18n2_04.dat"));
-  stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC17n4_04.dat"));
-
-	
+//  stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE18n2_04.dat"));
+//  stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC17n4_04.dat"));
+/*
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE17n2_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIB16n4_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE17n3_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC17n3_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE17n4_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC18n2_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE17n5_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC19n1_04.dat"));
+//*/
+/*
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE18n1_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIB16n5_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE18n2_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC17n4_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE18n3_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC18n3_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE18n4_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC19n2_04.dat"));
+//*/
+/*
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE18n5_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC20n1_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE19n1_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC17n5_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE19n2_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC18n4_04.dat"));
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE19n3_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC19n3_04.dat"));
+//*/
+//*
+stubinnerfiles.push_back(new ifstream("VMStubs_VMSTE_L1PHIE19n4_04.dat"));
+stubouterfiles.push_back(new ifstream("VMStubs_VMSTE_L2PHIC20n2_04.dat"));
+//*/	
   ofstream* memfile = new ofstream("memfile_TE.dat");
 
-  const unsigned int NMODULES = stubinnerfiles.size();
-  for(unsigned int ifile=0; ifile<NMODULES; ifile++) { 
+  const uint32_t NMODULES = stubinnerfiles.size();
+  for(uint32_t ifile=0; ifile<NMODULES; ifile++) { 
     assert(stubinnerfiles[ifile]->is_open() && stubouterfiles[ifile]->is_open());
   }
 
 
-  const unsigned int nevents = NEVENTS;
+  const uint32_t nevents = NEVENTS;
   string line;
   vector<uint32_t> rxdata0;
   vector<uint32_t> htmpinner[4];
   vector<uint32_t> htmpouter[4];
-  for(unsigned int ifile=0; ifile<NMODULES; ifile++) {
-    unsigned int membank=0;
+  for(uint32_t ifile=0; ifile<NMODULES; ifile++) {
+    uint32_t membank=0;
 
     htmpinner[0].push_back(0x0);
     htmpinner[1].push_back(0x0);
@@ -71,12 +103,12 @@ int main(int argc, char ** argv) {
   // fill between prefix and number with 0s
   cout << std::internal << std::setfill('0');
 
-  unsigned int ievt=0;
-  vector<unsigned int> ievtinner, ievtouter;
-  unsigned int iheadinner=0, iheadouter=0;
+  uint32_t ievt=0;
+  vector<uint32_t> ievtinner, ievtouter;
+  uint32_t iheadinner=0, iheadouter=0;
   vector<bool> eofinner, eofouter;
 
-  for(unsigned int ifile=0; ifile<NMODULES; ifile++) {
+  for(uint32_t ifile=0; ifile<NMODULES; ifile++) {
     ievtinner.push_back(0);
     ievtouter.push_back(0);
     eofinner.push_back(false);
@@ -86,8 +118,8 @@ int main(int argc, char ** argv) {
   while(ievt < nevents) {
     ievt++;
 
-    for(unsigned int ifile=0; ifile<NMODULES; ifile++) {
-      unsigned int nstubinner=0;
+    for(uint32_t ifile=0; ifile<NMODULES; ifile++) {
+      uint32_t nstubinner=0;
       while(ievtinner[ifile] <= ievt && !eofinner[ifile]) {
         if(getline(*stubinnerfiles[ifile], line)) {
           if(line.find("Event") != string::npos) {
@@ -97,7 +129,7 @@ int main(int argc, char ** argv) {
           } else {
             // Parse the line in the data file
             // For inner stubs, we only need the stub data (last column)
-            unsigned long stubdata = strtoul(split(line,' ').back().c_str(),NULL,0);
+            uint64_t stubdata = strtoul(split(line,' ').back().c_str(),NULL,0);
             if(nstubinner==0) {  // header word before stub data words to follow
               iheadinner = rxdata0.size();
               rxdata0.push_back(htmpinner[0][ifile]);
@@ -119,21 +151,21 @@ int main(int argc, char ** argv) {
         }
       }
 
-      unsigned int nstubouter=0;
-      unsigned int nstubouterZ[8] = {0,0,0,0,0,0,0,0};
+      uint64_t nstubouter=0;
+      uint64_t nstubouterZ[8] = {0,0,0,0,0,0,0,0};
       while(ievtouter[ifile] <= ievt && !eofouter[ifile]) {
         if(getline(*stubouterfiles[ifile], line)) {
           if(line.find("Event") != string::npos) {
             ievtouter[ifile]++;
             nstubouter=0;
-            for(unsigned int k=0; k<8; k++) { nstubouterZ[k]=0; }
+            for(uint32_t k=0; k<8; k++) { nstubouterZ[k]=0; }
 
           } else {
             // Parse the line in the data file
             // For outer stubs, we need the z-bin slot and the stub data (first and last column, respectively)
             vector<string> tokens = split(line,' ');
-            unsigned long zbin     = strtoul(tokens.front().c_str(),NULL,0);
-            unsigned long stubdata = strtoul(tokens.back().c_str(),NULL,0);
+            uint64_t zbin     = strtoul(tokens.front().c_str(),NULL,0);
+            uint64_t stubdata = strtoul(tokens.back().c_str(),NULL,0);
             if(nstubouter==0) {  // header word before stub data words to follow
               iheadouter = rxdata0.size();
               rxdata0.push_back(htmpouter[0][ifile]);
@@ -149,8 +181,8 @@ int main(int argc, char ** argv) {
             nstubouterZ[zbin]++;
 
             // update header word with number of stubs
-            unsigned long nstubouterZ_word = 0;
-            for(unsigned int k=0; k<8; k++) {
+            uint64_t nstubouterZ_word = 0;
+            for(uint32_t k=0; k<8; k++) {
               nstubouterZ_word += (nstubouterZ[k]<<(k*5));
             }
             rxdata0[iheadouter]   = ( htmpouter[0][ifile] | (nstubouterZ_word       & 0xFFFF) );
@@ -165,7 +197,7 @@ int main(int argc, char ** argv) {
     }
   }
 
-  for(unsigned int i=0; i<rxdata0.size()/4; i++) {
+  for(uint32_t i=0; i<rxdata0.size()/4; i++) {
     cout << std::hex << std::setw(4) << rxdata0[4*i+3];
     cout << " "      << std::setw(4) << rxdata0[4*i+2];
     cout << " "      << std::setw(4) << rxdata0[4*i+1];
