bd8341abe3297ad10401873fc023de8ecfb6027d
Kristian Hahn <khahn@cern.ch>
2021-09-12 20:02:29 -0500

    decrementing payload length

--- scripts/eth_payload.sh
+++ scripts/eth_payload.sh
@@ -9,6 +9,7 @@ regtool.exe -dx0 -reth10g.ctrl.reg.echo  0
 # configue the payload generator
 regtool.exe -dx0 -rpayload.ctrl.pause_interval 0x1
 regtool.exe -dx0 -rpayload.ctrl.pkt_len 0xFF
+# regtool.exe -dx0 -rpayload.ctrl.reg.decrement_len 0x1
 
 # configure the eth interface
 regtool.exe -dx0 -reth10g.ctrl.local_ip C0A80253
