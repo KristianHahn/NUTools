06d1435aea127b0885cd1677e3d6c51ec3097185
Kristian Hahn <khahn@cern.ch>
2021-09-15 00:00:28 -0500

    x0 -> x1 for tif

--- scripts/eth_echo.sh
+++ scripts/eth_echo.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # diable payload generation
-regtool.exe -dx0 -rpayload.ctrl.reg.en 0
+regtool.exe -dx1 -rpayload.ctrl.reg.en 0
 
 # configure th interface
-regtool.exe -dx0 -reth10g.ctrl.local_ip C0A80352
-regtool.exe -dx0 -reth10g.ctrl.remote_ip C0A80302
-regtool.exe -dx0 -reth10g.ctrl.ports.local 04D2
-regtool.exe -dx0 -reth10g.ctrl.ports.remote 04D2
+regtool.exe -dx1 -reth10g.ctrl.local_ip C0A80352
+regtool.exe -dx1 -reth10g.ctrl.remote_ip C0A80302
+regtool.exe -dx1 -reth10g.ctrl.ports.local 04D2
+regtool.exe -dx1 -reth10g.ctrl.ports.remote 04D2
 
 # enable the echo server
-regtool.exe -dx0 -reth10g.ctrl.reg.echo  1
+regtool.exe -dx1 -reth10g.ctrl.reg.echo  1
 
--- scripts/eth_payload.sh
+++ scripts/eth_payload.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 # disable payload generation
-regtool.exe -dx0 -rpayload.ctrl.reg.en 0
+regtool.exe -dx1 -rpayload.ctrl.reg.en 0
 
 # disable the echo server 
-regtool.exe -dx0 -reth10g.ctrl.reg.echo  0
+regtool.exe -dx1 -reth10g.ctrl.reg.echo  0
 
 # configue the payload generator
-regtool.exe -dx0 -rpayload.ctrl.pause_interval 0x1
-regtool.exe -dx0 -rpayload.ctrl.pkt_len 0xFF
-# regtool.exe -dx0 -rpayload.ctrl.reg.decrement_len 0x1
+regtool.exe -dx1 -rpayload.ctrl.pause_interval 0x1
+regtool.exe -dx1 -rpayload.ctrl.pkt_len 0xFF
+# regtool.exe -dx1 -rpayload.ctrl.reg.decrement_len 0x1
 
 # configure the eth interface
-regtool.exe -dx0 -reth10g.ctrl.local_ip C0A80352
-regtool.exe -dx0 -reth10g.ctrl.remote_ip C0A80302
-regtool.exe -dx0 -reth10g.ctrl.ports.local 04D2
-regtool.exe -dx0 -reth10g.ctrl.ports.remote 04D2
+regtool.exe -dx1 -reth10g.ctrl.local_ip C0A80352
+regtool.exe -dx1 -reth10g.ctrl.remote_ip C0A80302
+regtool.exe -dx1 -reth10g.ctrl.ports.local 04D2
+regtool.exe -dx1 -reth10g.ctrl.ports.remote 04D2
 
 # enable payload generation
-regtool.exe -dx0 -rpayload.ctrl.reg.en 1
+regtool.exe -dx1 -rpayload.ctrl.reg.en 1
 
 
 
